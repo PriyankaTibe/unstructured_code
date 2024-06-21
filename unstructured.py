46f34ee330fb03eef5cc36333cc89b4933cce2d8
Priyanka <tibepriyanka99@gmail.com>
2024-06-21 23:15:41 +0530

    create dev branch

--- unstructured.py
+++ unstructured.py
@@ -1,4 +1,4 @@
 from unstructured.partition.pdf import partition_pdf
-# elements = partition_pdf("gpt4all.pdf", strategy="hi_res")
+elements = partition_pdf("gpt4all.pdf", strategy="hi_res")
 elements = partition_pdf("gpt4all.pdf")
 
